427e314fb4c5ada7bff634f0f7725cfa9a0fe534
Sarah Trowbridge <sarah.trowbridge@joinkoru.com>
2018-08-13 11:16:51 -0700

    Can get response from Postman

--- index.js
+++ index.js
@@ -1,13 +1,62 @@
 const serverless = require('serverless-http');
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const bodyParser = require("body-parser");
+const queryParser = require('query-string');
+// 
+// const hook = {
+//     handler:
+// }
+
+
+app.use(express.json());
+
+// app.use(bodyParser.urlencoded({ extended: false }));
 
 app.post('/complete', function (req, res) {
-  res.send('sup')
+
+
+    res.send(`You sent ${response} to Express`);
 })
 
-// app.get('/complete', function (req, res){
-//     console.log(req, res, "you've made it!");
-// })
+isJsonString: str =>
+    (typeof str === 'string' &&
+    str.trim().slice(0, 1) === '{' &&
+    str.trim().slice(-1) === '}'),
 
+parseBody: event => {
+    let body;
+
+    if (event && 'body' in event) {
+      if (typeof event.body === 'string') {
+        if (utils.isJsonString(event.body)) {
+          try {
+            body = JSON.parse(event.body);
+          } catch (err) {
+            throw err;
+          }
+        } else if (/=|\&/g.test(event.body)) {
+          body = queryParser.parse(event.body);
+        } else {
+          body = event.body;
+        }
+      } else if (typeof event.body === 'object' && !Array.isArray(event.body)) {
+        body = event.body;
+      } else {
+        body = {};
+      }
+    }
+    if (!body) {
+      winston.error(`[utils][parseBody] - I got an event with no body,
+        which seems strange - %j`, event);
+      body = event;
+    }
+    return body;
+},
+
+/** when survey gizmo makes that request:
+ - parse it so that the app can handle it - look at utils /parseBody method in sg-hooks
+ - send it to Josh
+
+**/
 module.exports.handler = serverless(app);
